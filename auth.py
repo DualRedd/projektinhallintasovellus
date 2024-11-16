57ebe7b57752416a331d4be9022f32d5e3d9e0dd
Jonni Haapiainen <jonni.haapiainen@gmail.com>
2024-11-16 19:49:52 +0200

    auth.py small changes

--- auth.py
+++ auth.py
@@ -3,8 +3,11 @@ from sqlalchemy.sql import text
 from werkzeug.security import check_password_hash, generate_password_hash
 
 def get_user(username : str):
-    user = db.session.execute(text("SELECT id, password FROM users WHERE username = :username"), {"username":username}).fetchone()
+    user = db.session.execute(text("SELECT id, username, password FROM users WHERE username = :username"), {"username":username}).fetchone()
     return user
+
+def user_exists(username : str) -> bool:
+    return get_user(username) != None
 
 def authenticate_user(username : str, password : str) -> bool:
     user = get_user(username)
@@ -12,10 +15,7 @@ def authenticate_user(username : str, password : str) -> bool:
         return False # username does not exist in database
     return check_password_hash(user.password, password)
 
-def user_exists(username : str) -> bool:
-    return get_user(username) != None
-
-def add_user(username : str, password : str):
+def create_user(username : str, password : str):
     password_hash = generate_password_hash(password)
     db.session.execute(text("INSERT INTO users (username, password) VALUES (:username, :password)"),
                        {"username":username, "password":password_hash})
